af410d6a3b0c54fc97f83d145b77e348e0541c8b
allen-xavier <allengx@msn.com>
2020-10-30 15:22:47 -0300

    projeto inicial

---
+++ pris/src/main/java/br/com/pris/pris/PrisApplication.java
@@ -0,0 +1,13 @@
+package br.com.pris.pris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrisApplication.class, args);
+	}
+
+}
---
+++ pris/src/test/java/br/com/pris/pris/PrisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pris.pris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
