0344e7e9801b28648ac841ad602f20bc474f6bcd
allen-xavier <allengx@msn.com>
2020-11-15 21:14:33 -0300

    finalizado backEnd. nos achamos. kkkk

--- pris/src/main/java/br/com/pris/pris/model/services/InventoryService.java
+++ pris/src/main/java/br/com/pris/pris/model/services/InventoryService.java
@@ -3,45 +3,27 @@ package br.com.pris.pris.model.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.pris.pris.model.entities.ItemPedido;
 import br.com.pris.pris.model.entities.Material;
-import br.com.pris.pris.model.entities.MaterialProduto;
-import br.com.pris.pris.model.entities.Pedido;
+import br.com.pris.pris.model.repositories.MaterialProdutoRepository;
 
 @Service
 public class InventoryService {
-
+	
 	@Autowired
-	private PedidoService pedidos;
+	private MaterialProdutoRepository materialProduto;
 	
 	@Autowired
 	private MaterialService materiais;
 
-	public void returnsMaterial(Integer idPedido) {
-		Pedido pedido = this.pedidos.findPedidoById(idPedido);
-
-		for (ItemPedido produto : pedido.getProdutos()) {
-			Integer qtdProduto = produto.getQuantidade();
-			for (MaterialProduto material : produto.getProduto().getMateriais()) {
-				Material newMaterial = material.getMaterial();			
-				Integer qtdMaterial = material.getQuantidade();
-				newMaterial.setEstoque(newMaterial.getEstoque()+(qtdProduto*qtdMaterial));
-				this.materiais.changeMaterial(newMaterial, newMaterial.getIdMaterial());
-			}
-		}
+	public void returnsMaterial(Integer idProd, Integer quantidade) {
+		Material newMaterial = materiais.findMaterialById(materialProduto.idMaterial(idProd));
+		newMaterial.setEstoque(materialProduto.somaEstoque(idProd, quantidade));
+		this.materiais.changeMaterial(newMaterial, newMaterial.getIdMaterial());
 	}
 
-	public void removeMaterial(Integer idPedido) {
-		Pedido pedido = this.pedidos.findPedidoById(idPedido);
-
-		for (ItemPedido produto : pedido.getProdutos()) {
-			Integer qtdProduto = produto.getQuantidade();
-			for (MaterialProduto material : produto.getProduto().getMateriais()) {
-				Material newMaterial = material.getMaterial();			
-				Integer qtdMaterial = material.getQuantidade();
-				newMaterial.setEstoque(newMaterial.getEstoque()-(qtdProduto*qtdMaterial));
-				this.materiais.changeMaterial(newMaterial, newMaterial.getIdMaterial());
-			}
-		}
+	public void removeMaterial(Integer idProd, Integer quantidade) {
+		Material newMaterial = materiais.findMaterialById(materialProduto.idMaterial(idProd));
+		newMaterial.setEstoque(materialProduto.retiraEstoque(idProd, quantidade));
+		this.materiais.changeMaterial(newMaterial, newMaterial.getIdMaterial());
 	}
 }
--- pris/src/main/java/br/com/pris/pris/model/services/ItemPedidoService.java
+++ pris/src/main/java/br/com/pris/pris/model/services/ItemPedidoService.java
@@ -19,9 +19,32 @@ public class ItemPedidoService {
 	@Autowired
 	private ItemPedidoRepository repository;
 	
+	@Autowired
+	private InventoryService inventory;
+	
 
 	public ItemPedido addItemPedido(@Valid ItemPedido itemPedido) {
+		if (itemPedido.getIdItemPedido() > 0) {
+			if (findItemPedidoById(itemPedido.getIdItemPedido()).getQuantidade() < itemPedido.getQuantidade()) {
+				Integer mudanca = itemPedido.getQuantidade() - findItemPedidoById(itemPedido.getIdItemPedido()).getQuantidade();
+				this.repository.save(itemPedido);
+				this.inventory.returnsMaterial(itemPedido.getIdProduto(), mudanca);
+				return itemPedido;
+			}
+			if (findItemPedidoById(itemPedido.getIdItemPedido()).getQuantidade() > itemPedido.getQuantidade()) {
+				Integer mudanca = findItemPedidoById(itemPedido.getIdItemPedido()).getQuantidade() - itemPedido.getQuantidade();
+				this.repository.save(itemPedido);
+				this.inventory.removeMaterial(itemPedido.getIdProduto(), mudanca);
+				return itemPedido;
+			}
+			if (findItemPedidoById(itemPedido.getIdItemPedido()).getQuantidade() == itemPedido.getQuantidade()) {
+				return this.repository.save(itemPedido);
+			}
+		}
+		
+		this.inventory.returnsMaterial(itemPedido.getIdProduto(), itemPedido.getQuantidade());
 		return this.repository.save(itemPedido);
+		
 	}
 
 	public Iterable<ItemPedido> findAllItemPedidos() {
@@ -41,7 +64,13 @@ public class ItemPedidoService {
 
 	public void deleteItemPedido(Integer id) {
 		try {
-			repository.deleteById(id);
+			if (repository.existsById(id)) {
+					ItemPedido itemPedido = repository.findById(id).get();
+					Integer mudanca = itemPedido.getQuantidade();
+					this.inventory.returnsMaterial(itemPedido.getIdProduto(), mudanca);
+					this.repository.deleteById(id);
+				}
+			//repository.deleteById(id);
 			}catch(EmptyResultDataAccessException e){
 				throw new ResponseStatusException(HttpStatus.NOT_FOUND,
 						"O ItemPedido não foi encontrado.");
